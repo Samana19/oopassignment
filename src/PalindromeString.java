d18d286a5dded3e517193f4d8f8551e23f85e9a0
Samana Shrestha <samanashrestha02@gmail.com>
2021-12-07 17:49:29 +0545

    Initial commit

---
+++ src/PalindromeString.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String palindrome;
+        String rev="";
+        int length;
+
+        System.out.println("Enter a string val: ");
+        palindrome=sc.nextLine();
+
+        length=palindrome.length();
+        for ( int i = length - 1; i >= 0; i-- )
+            rev = rev + palindrome.toLowerCase().charAt(i);
+
+        if (palindrome.equals(rev))
+            System.out.println(palindrome+" is a palindrome");
+        else
+            System.out.println(palindrome+" is not a palindrome");
+
+    }
+}
---
+++ src/PrimeNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int k;
+        int i;
+
+        System.out.println("Enter an integer value :");
+        num = sc.nextInt();
+
+        for (i = 2; i <= num; i++) {
+            k = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    k = k + 1;
+                }
+            }
+
+            if (k <= 2) {
+                System.out.println(i);
+            }
+
+        }
+
+
+    }
+}
